52fd433b80377be9f0c77acacf69eb8e57405666
Kamnev Yuriy <yury.kamnev@microavia.com>
2021-05-08 21:47:20 +0300

    Refactor Storage class

--- port/cpp/messgen/Storage.h
+++ port/cpp/messgen/Storage.h
@@ -6,32 +6,27 @@
 
 namespace messgen {
 
-template<class T, size_t SIZE=0, bool D = T::HAS_DYNAMICS>
-class Storage {};
-
 /**
  *  @brief Class for storing objects with dynamic fields.
  *          Owns memory for dynamic allocations.
  */
-template <class T, size_t SIZE>
-class Storage<T, SIZE, true> {
+template<class T>
+class StorageBase {
 public:
-    static_assert(SIZE != 0, "Storage size for message with dynamic fields has zero length!");
-
     static constexpr uint8_t TYPE = T::TYPE;
     static constexpr size_t STATIC_SIZE = T::STATIC_SIZE;
     static constexpr uint8_t PROTO = T::PROTO;
     static constexpr bool HAS_DYNAMICS = T::HAS_DYNAMICS;
 
-    Storage() {
-        std::cout << "Dynamic storage!" << std::endl;
-    }
+    StorageBase() noexcept = default;
 
-    explicit Storage(const T& value) noexcept:
-        _value(value) {}
+    virtual ~StorageBase() = default;
 
-    explicit Storage(const T&& value) noexcept:
-        _value(value) {}
+    explicit StorageBase(const T& value) noexcept:
+            _value(value) {}
+
+    explicit StorageBase(const T&& value) noexcept:
+            _value(value) {}
 
     bool operator== (const T& rhs) const noexcept {
         return _value == rhs;
@@ -49,12 +44,12 @@ public:
         return &_value;
     }
 
-    Storage& operator= (const T& rhs) noexcept {
+    StorageBase& operator= (const T& rhs) noexcept {
         _value = rhs;
         return *this;
     }
 
-    Storage& operator= (const T&& rhs) noexcept {
+    StorageBase& operator= (const T&& rhs) noexcept {
         _value = rhs;
         return *this;
     }
@@ -75,86 +70,64 @@ public:
         return _value.get_dynamic_size();
     }
 
-    int parse_msg(const uint8_t *buf, uint32_t len) noexcept {
-        return _value.parse_msg(buf, len, _memory_allocator);
-    }
-
     int serialize_msg(uint8_t *buf) const noexcept {
         return _value.serialize_msg(buf);
     }
 
+protected:
+    T _value;
+};
+
+template<class T, size_t SIZE=0, bool D = T::HAS_DYNAMICS>
+class Storage {};
+
+/**
+ *  @brief Class for storing objects with dynamic fields.
+ *          Owns memory for dynamic allocations.
+ */
+template <class T, size_t SIZE>
+class Storage<T, SIZE, true> : public StorageBase<T> {
+public:
+    static_assert(SIZE != 0, "Storage size for message with dynamic fields has zero length!");
+
+    Storage() noexcept = default;
+
+    explicit Storage(const T& value) noexcept:
+            StorageBase<T>(value) {}
+
+    explicit Storage(const T&& value) noexcept:
+            StorageBase<T>(value) {}
+
+    int parse_msg(const uint8_t *buf, uint32_t len) noexcept {
+        return this->_value.parse_msg(buf, len, _memory_allocator);
+    }
+
 private:
-    mutable T _value;
     StaticMemoryAllocator<SIZE> _memory_allocator;
 };
 
 /**
- *  @brief Class for storing objects without dynamic fields
+ *  @brief Class for storing objects with dynamic fields.
+ *          Owns memory for dynamic allocations.
  */
 template <class T, size_t SIZE>
-class Storage<T, SIZE, false> {
+class Storage<T, SIZE, false> : public StorageBase<T> {
 public:
     static_assert(SIZE == 0, "Storage for message without dynamic fields has non zero length!");
 
-    static constexpr uint8_t TYPE = T::TYPE;
-    static constexpr size_t STATIC_SIZE = T::STATIC_SIZE;
-    static constexpr uint8_t PROTO = T::PROTO;
-    static constexpr bool HAS_DYNAMICS = T::HAS_DYNAMICS;
-
-    Storage() {
-        std::cout << "Simple Storage" << std::endl;
-    }
+    Storage() noexcept = default;
 
     explicit Storage(const T& value) noexcept:
-            _value(value) {}
+            StorageBase<T>(value) {}
 
     explicit Storage(const T&& value) noexcept:
-            _value(value) {}
-
-    T* operator->() noexcept {
-        return &_value;
-    }
-
-    const T* operator->() const noexcept {
-        return &_value;
-    }
-
-    Storage& operator= (const T& rhs) noexcept {
-        _value = rhs;
-        return *this;
-    }
-
-    Storage& operator= (const T&& rhs) noexcept {
-        _value = rhs;
-        return *this;
-    }
-
-    operator T& () noexcept {
-        return _value;
-    }
-
-    operator const T& () const noexcept {
-        return _value;
-    }
-
-    size_t get_size() const noexcept {
-        return _value.get_size();
-    }
-
-    size_t get_dynamic_size() const noexcept {
-        return _value.get_dynamic_size();
-    }
+            StorageBase<T>(value) {}
 
     int parse_msg(const uint8_t *buf, uint32_t len) noexcept {
-        return _value.parse_msg(buf, len, _memory_allocator);
-    }
-
-    int serialize_msg(uint8_t *buf) const noexcept {
-        return _value.serialize_msg(buf);
+        return this->_value.parse_msg(buf, len, _memory_allocator);
     }
 
 private:
-    T _value;
     MemoryAllocator _memory_allocator{nullptr, 0};
 };
 
