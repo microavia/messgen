95f020829c588d36a8a545ae349ec4900c19e66c
Kirill Baldin <kirill.baldin@alberblanc.com>
2023-09-01 12:33:33 +0400

    Add configuration files and refactor coding style with prettier.

---
+++ port/js/prettier.config.js
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+const config = {
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  printWidth: 120,
+  trailingComma: 'none',
+  semi: false
+}
+module.exports = config
