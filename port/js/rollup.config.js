feeba3a3654e99f25100bd7826d2d88eaf20f8e9
Kirill Baldin <kirill.baldin@alberblanc.com>
2023-09-01 15:31:04 +0400

    Update tab spacing in JS files

--- port/js/rollup.config.js
+++ port/js/rollup.config.js
@@ -5,16 +5,16 @@ const OUTPUT_NAME = `dist/messgen`
 
 /** @type {import('rollup').RollupOptions} */
 const config = {
-  input: `src/messgen.js`,
-  output: [
-    {
-      file: OUTPUT_NAME + '.js',
-      format: 'umd',
-      name: pkg.name,
-      sourcemap: false
-    }
-  ],
-  plugins: [terser()]
+    input: `src/messgen.js`,
+    output: [
+        {
+            file: OUTPUT_NAME + '.js',
+            format: 'umd',
+            name: pkg.name,
+            sourcemap: false
+        }
+    ],
+    plugins: [terser()]
 }
 
 export default config
