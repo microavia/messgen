1d8ee8c69a352349b9dc05c0cc07c7f058092971
Anton Babushkin <anton.babushkin@me.com>
2023-10-05 18:40:13 +0200

    Old files cleanup

--- messgen/generator.py
+++ messgen/generator.py
@@ -1,10 +1,11 @@
-from . json_generator2 import JsonGenerator
-from . cpp_generator2 import CppGenerator
+from .json_generator import JsonGenerator
+from .cpp_generator import CppGenerator
 
 _GENERATORS = {
     "json": JsonGenerator,
     "cpp": CppGenerator,
 }
+
 
 def get_generator(lang: str):
     return _GENERATORS.get(lang)
--- messgen/messgen_ex.py
+++
@@ -1,5 +0,0 @@
-
-
-class MessgenException(Exception):
-    def __init__(self, message):
-        super().__init__(message)
