823c36f32245ab55d68eaae57751eda1602195cf
Anton Babushkin <anton.babushkin@microavia.com>
2023-12-24 17:35:59 +0100

    Cmake improvements and fixes, minor refactoring

--- tests/cpp_stl/CppTest.h
+++ tests/cpp_stl/CppTest.h
@@ -5,6 +5,7 @@
 #include <messgen/test_proto/struct_with_enum.h>
 #include <messgen/test_proto/var_size_struct.h>
 #include <messgen/test_proto/flat_struct.h>
+#include <another_proto.h>
 
 #include <gtest/gtest.h>
 
