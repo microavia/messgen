4b04d7bb0a018d5c2b28bdba49d9ce667239d022
lachem <adamlach@gmail.com>
2024-08-06 13:05:39 +0200

    Update cpp_generator.py

--- messgen/cpp_generator.py
+++ messgen/cpp_generator.py
@@ -130,7 +130,8 @@ class CppGenerator:
 
         proto_id = proto["proto_id"]
         if proto_id is not None:
-            code.append("static constexpr int PROTO_ID = %s;\n" % proto_id)
+            code.append("static constexpr int PROTO_ID = %s;" % proto_id)
+            code.append()
 
         for type_name in proto.get("types"):
             type_def = self._protocols.get_type(proto_name, type_name)
