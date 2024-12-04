ee54456c260deafa7873bb5e0d239bc78e190836
Adam Lach <adam.lach@alberblanc.com>
2024-12-04 17:47:23 +0100

    Extract concepts to separate files

--- port/cpp_stl/messgen/messgen.h
+++ port/cpp_stl/messgen/messgen.h
@@ -1,7 +1,8 @@
 #pragma once
 
+#include "concepts.h"
+
 #include <array>
-#include <concepts>
 #include <cstdint>
 #include <map>
 #include <string>
@@ -143,22 +144,5 @@ template <class K, class V>
 }
 
 using size_type = uint32_t;
-
-template <class Msg>
-concept serializable = requires(std::remove_cvref_t<Msg> msg, uint8_t *buf) {
-    { msg.serialized_size() } -> std::same_as<size_t>;
-    { msg.serialize(buf) } -> std::same_as<size_t>;
-    { msg.deserialize(buf) } -> std::same_as<size_t>;
-};
-
-template <class Msg>
-concept message = serializable<Msg> && requires(std::remove_cvref_t<Msg> msg) {
-    { msg.NAME } -> std::convertible_to<const char *>;
-    { msg.SCHEMA } -> std::convertible_to<const char *>;
-    { msg.IS_FLAT } -> std::convertible_to<bool>;
-};
-
-template <class Msg>
-concept flat_message = message<Msg> && std::remove_cvref_t<Msg>::IS_FLAT;
 
 } // namespace messgen
