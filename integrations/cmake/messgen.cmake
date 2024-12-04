919d2323a37aaedf5f06efa0185d97604463445a
Adam Lach <adam.lach@alberblanc.com>
2024-12-04 17:30:21 +0100

    Adopt cmake files to the new approach

--- integrations/cmake/messgen.cmake
+++ integrations/cmake/messgen.cmake
@@ -3,70 +3,35 @@
 # Searches for PROTOCOLS in BASE_DIRS and generate files in OUTDIR.
 # List of generated files provided in OUTFILES_VAR variable.
 #
-function(messgen_generate_messages BASE_DIRS PROTOCOLS OUTDIR OUTFILES_VAR OPTIONS)
+function(messgen_generate_types BASE_DIRS OUTDIR OUTFILES_VAR OPTIONS)
     get_filename_component(MESSGEN_DIR ${CMAKE_CURRENT_FUNCTION_LIST_DIR} DIRECTORY)
     get_filename_component(MESSGEN_DIR ${MESSGEN_DIR} DIRECTORY)
     set(MESSGEN_INPUT_FILES "")
     set(OUTFILES "")
 
-    list(REMOVE_DUPLICATES PROTOCOLS)
-
     set(MESSGEN_ARGS "")
     foreach (BASE_DIR ${BASE_DIRS})
-        list(APPEND MESSGEN_ARGS "--basedir")
-        list(APPEND MESSGEN_ARGS ${BASE_DIR})
+        list(APPEND MESSGEN_ARGS "--types" ${BASE_DIR})
     endforeach ()
 
-    foreach (PROTOCOL ${PROTOCOLS})
-        set(FOUND FALSE)
-        foreach (BASE_DIR ${BASE_DIRS})
-            set(MESSAGES_PATH ${BASE_DIR}/${PROTOCOL})
-            if (EXISTS ${MESSAGES_PATH})
-                set(FOUND TRUE)
+    foreach (TYPES_PATH ${BASE_DIRS})
+        if (EXISTS ${TYPES_PATH})
+            # This will trigger reconfiguration when messages definition changes
+            # However this requires CMake >= 3.0. Need to change all configs with minimum required version
+            set_property(DIRECTORY APPEND PROPERTY CMAKE_CONFIGURE_DEPENDS ${TYPES_PATH})
 
-                set(MESSAGES_OUTDIR ${OUTDIR}/${PROTOCOL})
+            file(GLOB_RECURSE TYPES RELATIVE ${TYPES_PATH} ${TYPES_PATH}/*.yaml)
 
-                # This will trigger reconfiguration when messages definition changes
-                # However this requires CMake >= 3.0. Need to change all configs with minimum required version
-                set_property(
-                        DIRECTORY
-                        APPEND
-                        PROPERTY CMAKE_CONFIGURE_DEPENDS
-                        ${MESSAGES_PATH}
-                )
+            if (TYPES)
+                foreach (TYPE_YAML ${TYPES})
+                    string(REGEX REPLACE "(yaml)" "h" MSG_HEADER ${TYPE_YAML})
+                    string(REGEX REPLACE "(yaml)" "cpp" MSG_CPP ${TYPE_YAML})
 
-                file(GLOB PROTOCOL_MESSAGES RELATIVE ${MESSAGES_PATH} ${MESSAGES_PATH}/*.yaml)
-                list(REMOVE_ITEM PROTOCOL_MESSAGES "_protocol.yaml")
-
-                if (PROTOCOL_MESSAGES)
-                    foreach (MSG_YAML ${PROTOCOL_MESSAGES})
-                        string(REGEX REPLACE "(yaml)" "h" MSG_HEADER ${MSG_YAML})
-                        string(REGEX REPLACE "(yaml)" "cpp" MSG_CPP ${MSG_YAML})
-
-                        list(APPEND MESSGEN_INPUT_FILES ${MESSAGES_PATH}/${MSG_YAML})
-
-                        list(APPEND OUTFILES ${MESSAGES_OUTDIR}/${MSG_HEADER})
-                    endforeach ()
-                endif ()
-
-                list(APPEND MESSGEN_INPUT_FILES
-                        ${MESSAGES_PATH}/_protocol.yaml
-                )
-
-                get_filename_component(PROTOCOL_BASE ${MESSAGES_OUTDIR} DIRECTORY)
-                get_filename_component(PROTOCOL_NAME ${PROTOCOL} NAME)
-                list(APPEND OUTFILES
-                        ${PROTOCOL_BASE}/${PROTOCOL_NAME}.h
-                )
+                    list(APPEND MESSGEN_INPUT_FILES ${TYPES_PATH}/${TYPE_YAML})
+                    list(APPEND OUTFILES ${OUTDIR}/${MSG_HEADER})
+                endforeach ()
             endif ()
-        endforeach ()
-
-        if (NOT FOUND)
-            message(FATAL_ERROR "Protocol ${PROTOCOL} not found! Searched in: ${BASE_DIRS}")
         endif ()
-
-        list(APPEND MESSGEN_ARGS "--protocol")
-        list(APPEND MESSGEN_ARGS ${PROTOCOL})
     endforeach ()
 
     if (OPTIONS)
@@ -76,28 +41,91 @@ function(messgen_generate_messages BASE_DIRS PROTOCOLS OUTDIR OUTFILES_VAR OPTIO
 
     file(GLOB_RECURSE GENERATOR_DEPS ${MESSGEN_DIR}/*.py)
     add_custom_command(
-            OUTPUT ${OUTFILES}
-            COMMAND "python3"
-            ARGS
-            ${MESSGEN_DIR}/messgen-generate.py
-            ${MESSGEN_ARGS}
-            "--outdir" ${OUTDIR}
-            "--lang" "cpp"
-            DEPENDS ${GENERATOR_DEPS} ${MESSGEN_INPUT_FILES}
+        OUTPUT ${OUTFILES}
+        COMMAND "python3"
+        ARGS
+        ${MESSGEN_DIR}/messgen-generate.py
+        ${MESSGEN_ARGS}
+        "--outdir" ${OUTDIR}
+        "--lang" "cpp"
+        DEPENDS ${GENERATOR_DEPS} ${MESSGEN_INPUT_FILES}
     )
     set(${OUTFILES_VAR} ${OUTFILES} PARENT_SCOPE)
 endfunction()
 
+
+function(messgen_generate_protocol BASE_DIRS PROTOCOLS OUTDIR OUTFILES_VAR)
+    get_filename_component(MESSGEN_DIR ${CMAKE_CURRENT_FUNCTION_LIST_DIR} DIRECTORY)
+    get_filename_component(MESSGEN_DIR ${MESSGEN_DIR} DIRECTORY)
+    set(MESSGEN_INPUT_FILES "")
+    set(OUTFILES "")
+
+    foreach (BASE_DIR ${BASE_DIRS})
+        list(APPEND MESSGEN_ARGS "--protocols" ${BASE_DIR})
+    endforeach ()
+
+    foreach (PROTOCOLS_PATH ${BASE_DIRS})
+        if (EXISTS ${PROTOCOLS_PATH})
+            # This will trigger reconfiguration when messages definition changes
+            # However this requires CMake >= 3.0. Need to change all configs with minimum required version
+            set_property(DIRECTORY APPEND PROPERTY CMAKE_CONFIGURE_DEPENDS ${PROTOCOLS_PATH})
+
+            file(GLOB PROTOCOLS RELATIVE ${PROTOCOLS_PATH} ${PROTOCOLS_PATH}/*.yaml)
+
+            if (PROTOCOLS)
+                foreach (PROTO_YAML ${PROTOCOLS})
+                    string(REGEX REPLACE "(yaml)" "h" PROTO_HEADER ${PROTO_YAML})
+                    string(REGEX REPLACE "(yaml)" "cpp" PROTO_CPP ${PROTO_YAML})
+
+                    list(APPEND MESSGEN_INPUT_FILES ${PROTOCOLS_PATH}/${PROTO_YAML})
+                    list(APPEND OUTFILES ${OUTDIR}/${PROTO_HEADER})
+                endforeach ()
+            endif ()
+        endif ()
+    endforeach ()
+
+    file(GLOB_RECURSE GENERATOR_DEPS ${MESSGEN_DIR}/*.py)
+    add_custom_command(
+        OUTPUT ${OUTFILES}
+        COMMAND "python3"
+        ARGS
+        ${MESSGEN_DIR}/messgen-generate.py
+        ${MESSGEN_ARGS}
+        "--outdir" ${OUTDIR}
+        "--lang" "cpp"
+        DEPENDS ${GENERATOR_DEPS} ${MESSGEN_INPUT_FILES}
+    )
+    set(${OUTFILES_VAR} ${OUTFILES} PARENT_SCOPE)
+
+endfunction()
+
 #
-# Function creates a target for specified protocols.
+# Function creates a target for specified types.
 #
-function(messgen_add_library LIBRARY_NAME BASE_DIRS PROTOCOLS MODE)
+function(messgen_add_types_library LIBRARY_NAME BASE_DIRS MODE)
     string(JOIN "," OPTIONS "mode=${MODE}" ${ARGN})
-    set(MESSAGES_OUT_DIR "${CMAKE_BINARY_DIR}/${LIBRARY_NAME}/generated_src")
+    set(MESSAGES_OUT_DIR "${CMAKE_BINARY_DIR}/${LIBRARY_NAME}/generated_src/types")
     get_filename_component(MESSGEN_DIR ${CMAKE_CURRENT_FUNCTION_LIST_DIR} DIRECTORY)
     get_filename_component(MESSGEN_DIR ${MESSGEN_DIR} DIRECTORY)
     add_library(${LIBRARY_NAME} INTERFACE)
-    messgen_generate_messages("${BASE_DIRS}" "${PROTOCOLS}" "${MESSAGES_OUT_DIR}" MESSGEN_OUT_FILES "${OPTIONS}")
+    messgen_generate_types("${BASE_DIRS}" "${MESSAGES_OUT_DIR}" MESSGEN_OUT_FILES "${OPTIONS}")
     target_sources(${LIBRARY_NAME} INTERFACE ${MESSGEN_OUT_FILES})
-    target_include_directories(${LIBRARY_NAME} INTERFACE "${MESSAGES_OUT_DIR}" "${MESSGEN_DIR}/port/cpp_${MODE}")
+    target_include_directories(${LIBRARY_NAME} INTERFACE
+        ${MESSAGES_OUT_DIR}
+        ${MESSGEN_DIR}/port/cpp_${MODE})
+endfunction()
+
+#
+# Function creates a target for specified protocol.
+#
+function(messgen_add_protocol_library LIBRARY_NAME BASE_DIRS PROTOCOL TYPES_TARGET)
+    string(JOIN "," OPTIONS "mode=${MODE}" ${ARGN})
+    set(MESSAGES_OUT_DIR "${CMAKE_BINARY_DIR}/${LIBRARY_NAME}/generated_src/protocols")
+    get_filename_component(MESSGEN_DIR ${CMAKE_CURRENT_FUNCTION_LIST_DIR} DIRECTORY)
+    get_filename_component(MESSGEN_DIR ${MESSGEN_DIR} DIRECTORY)
+    add_library(${LIBRARY_NAME} INTERFACE)
+    messgen_generate_protocol("${BASE_DIRS}" "${PROTOCOL}" "${MESSAGES_OUT_DIR}" MESSGEN_OUT_FILES)
+    target_sources(${LIBRARY_NAME} INTERFACE ${MESSGEN_OUT_FILES})
+    target_include_directories(${LIBRARY_NAME} INTERFACE ${MESSAGES_OUT_DIR})
+    target_link_libraries(${LIBRARY_NAME} INTERFACE ${TYPES_TARGET})
 endfunction()
