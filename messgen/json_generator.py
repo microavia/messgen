b6e575050623050ed8569007974f047c22c964c9
Adam Lach <adam.lach@alberblanc.com>
2024-12-10 10:59:07 +0100

    Add type validation to avoid clashing hashes

--- messgen/json_generator.py
+++ messgen/json_generator.py
@@ -18,27 +18,19 @@ class JsonGenerator:
         self._options = options
 
     def generate_types(self, out_dir: Path, types: dict[str, MessgenType]) -> None:
-        combined: list = []
-
-        for type_def in types.values():
-            if type_def.type_class in [TypeClass.struct, TypeClass.enum]:
-                combined.append(asdict(type_def))
-
-        self._write_file(out_dir, "types", combined)
+        for type_name, type_def in types.items():
+            if type_def.type_class not in [TypeClass.struct, TypeClass.enum]:
+                continue
+            file_name = out_dir / (type_name + self._FILE_EXT)
+            file_name.parent.mkdir(parents=True, exist_ok=True)
+            type_dict = asdict(type_def)
+            type_dict["hash"] = hash(type_def)
+            write_file_if_diff(file_name, json.dumps(type_dict, indent=2).splitlines())
 
     def generate_protocols(self, out_dir: Path, protocols: dict[str, Protocol]) -> None:
         combined: list = []
-        
+
         for proto_def in protocols.values():
             proto_dict = asdict(proto_def)
-            proto_dict["version"] = version_hash(proto_dict)
-            combined.append(proto_dict)
-        
-        self._write_file(out_dir, "protocols", combined)
-
-    def _write_file(self, out_dir: Path, name: str, data: list) -> None:
-        file_name = out_dir / (name + self._FILE_EXT)
-        file_name.parent.mkdir(parents=True, exist_ok=True)
-
-        with open(file_name, "w", encoding="utf-8") as f: json.dump(data, f, indent=2)
-
+            proto_dict["hash"] = hash(proto_def)
+            write_file_if_diff(file_name, json.dumps(proto_dict, indent=2).splitlines())
