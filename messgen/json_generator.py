5426bc4564d6910828f92165d5c733a5d8e81c5a
Adam Lach <adam.lach@alberblanc.com>
2024-12-18 09:56:28 +0100

    Fix merge issue

--- messgen/json_generator.py
+++ messgen/json_generator.py
@@ -3,6 +3,7 @@ import json
 from dataclasses import asdict
 from pathlib import Path
 
+from .common import write_file_if_diff
 from .protocol_version import version_hash
 
 from .model import (
@@ -28,9 +29,9 @@ class JsonGenerator:
             write_file_if_diff(file_name, json.dumps(type_dict, indent=2).splitlines())
 
     def generate_protocols(self, out_dir: Path, protocols: dict[str, Protocol]) -> None:
-        combined: list = []
-
-        for proto_def in protocols.values():
+        for proto_name, proto_def in protocols.items():
+            file_name = out_dir / (proto_name + self._FILE_EXT)
+            file_name.parent.mkdir(parents=True, exist_ok=True)
             proto_dict = asdict(proto_def)
             proto_dict["hash"] = hash(proto_def)
             write_file_if_diff(file_name, json.dumps(proto_dict, indent=2).splitlines())
